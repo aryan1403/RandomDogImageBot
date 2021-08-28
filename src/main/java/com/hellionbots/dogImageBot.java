f4d222accbd9a0627afaea33dd80d13a9cbce6df
Aaryan <aaryan14032006@gmail.com>
2021-08-28 19:34:58 +0530

    Bot File created

---
+++ src/main/java/com/hellionbots/dogImageBot.java
@@ -0,0 +1,24 @@
+package com.hellionbots;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class dogImageBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        
+        
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+    
+}
