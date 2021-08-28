2bc9393df96ca844897f93c66c24c4fbae36287a
Aaryan <aaryan14032006@gmail.com>
2021-08-28 19:34:48 +0530

    Created App Runner

---
+++ src/main/java/com/hellionbots/App.java
@@ -0,0 +1,17 @@
+package com.hellionbots;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public final class App {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new dogImageBot());
+            System.out.println("Bot Started Successfully");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
